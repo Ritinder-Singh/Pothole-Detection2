bd0cb5e2b6919242acc359e073162f2e7dd00d9e
Ritinder Singh <for.ritindersingh@gmail.com>
2024-01-18 23:18:22 +0530

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 // const mongo_url = "mongodb+srv://admin:admin@cluster0.rysalpm.mongodb.net/?retryWrites=true&w=majority";
-const mongo_url = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1";
+const mongo_url = "";
 
  const connectToDB = async () => {
   try {
